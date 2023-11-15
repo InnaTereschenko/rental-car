a3e4daebc030eef94b955a07296e4d1c09c39b62
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-11-16 00:26:13 +0200

    create branch gh-pages

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import { lazy } from "react";
+import "./App.css";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
+
+const HomePage = lazy(() => import("pages/HomePage/HomePage"));
+const CatalogPage = lazy(() => import("pages/CatalogPage/CatalogPage"));
+const FavoritesPage = lazy(() => import("pages/FavoritesPage/FavoritesPage"));
+const NotFoundPage = lazy(() => import("pages/NotFoundPage/NotFoundPage"));
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="catalog" element={<CatalogPage />} />
+          <Route path="favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      {/* <ToastContainer /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,15 @@
+import { Main } from './SharedLayout.styled';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const SharedLayout = () => {
+    return (
+        <>
+        <Header />
+       <Main/>
+       <Footer/> 
+        </>
+    )
+} 
+
+export default SharedLayout;
