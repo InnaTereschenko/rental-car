f2e75e191da0cb2a1f81041db962f2081fd7b2a6
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-11-16 12:25:59 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { store, persistor } from 'redux/store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
